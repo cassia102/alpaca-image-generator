d4de170ce4402ed7ec9458c0894a58644ef2832a
cassia <cassiasadler96@gmail.com>
2023-12-05 17:07:05 +0000

    Updated url in puppeteer script

--- scripts/puppeteer-script.js
+++ scripts/puppeteer-script.js
@@ -3,7 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://8ae9-80-47-132-127.ngrok.io', { waitUntil: 'networkidle2' });
+  
+  await page.goto('https://e0b9-80-47-132-127.ngrok-free.app', { waitUntil: 'networkidle2' });
 
   const symbolDispose = Symbol('dispose');
   Symbol.dispose = Symbol.dispose || symbolDispose;
