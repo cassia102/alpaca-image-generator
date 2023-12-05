df1bae0db0eb952c39031317b5b992cd69803a8a
cassia <cassiasadler96@gmail.com>
2023-12-05 16:22:27 +0000

    Remove nullish coalescing assignment operator

--- scripts/puppeteer-script.js
+++ scripts/puppeteer-script.js
@@ -3,7 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://8ae9-80-47-132-127.ngrok.io');
+  await page.goto('http://8ae9-80-47-132-127.ngrok.io', { waitUntil: 'networkidle2' });
+
+  const symbolDispose = Symbol('dispose');
+  Symbol.dispose = Symbol.dispose || symbolDispose;
+
   await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
 })();
