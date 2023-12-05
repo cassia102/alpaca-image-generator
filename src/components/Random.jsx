c35190f209cd76f22dfb806bdda5bf1983fa2eec
cassia <cassiasadler96@gmail.com>
2023-12-05 16:59:56 +0000

    Add class to random button

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Random({handleRandom}) {
     return (
-        <button className="random-btn" onClick={handleRandom}><i className="icon-shuffle"></i>Random</button>   
+        <button className="random-btn" onClick={handleRandom}><i className="icon-shuffle icon"></i>Random</button>   
     )
 }
