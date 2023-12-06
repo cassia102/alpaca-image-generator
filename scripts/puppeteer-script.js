a0a6ee5fcc52a60d21a7330a61c3a57cb7a3e2f9
cassia <cassiasadler96@gmail.com>
2023-12-06 11:48:52 +0000

    Ensure headless mode

--- scripts/puppeteer-script.js
+++ scripts/puppeteer-script.js
@@ -1,7 +1,11 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+  });
+
   const page = await browser.newPage();
 
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0');
