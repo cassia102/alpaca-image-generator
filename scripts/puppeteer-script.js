6c466a74396dfdde301a1fdbebeed12cdef18cb7
cassia <cassiasadler96@gmail.com>
2023-12-06 11:46:13 +0000

    Add ngrok skip browser warning

--- scripts/puppeteer-script.js
+++ scripts/puppeteer-script.js
@@ -1,10 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0');
+
+  await page.setExtraHTTPHeaders({
+    'ngrok-skip-browser-warning': 'true',
+  });
 
   await page.goto('https://e0b9-80-47-132-127.ngrok-free.app', { waitUntil: 'networkidle2' });
 
