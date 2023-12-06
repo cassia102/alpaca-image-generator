db672348689a10d480ce9fd728f2818cb8402ff3
cassia <cassiasadler96@gmail.com>
2023-12-06 11:39:50 +0000

    Bypass Ngrok warning page by setUserAgent()

--- scripts/puppeteer-script.js
+++ scripts/puppeteer-script.js
@@ -3,7 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  
+
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
   await page.goto('https://e0b9-80-47-132-127.ngrok-free.app', { waitUntil: 'networkidle2' });
 
   const symbolDispose = Symbol('dispose');
